0869780dd72ac9910ecf07bdbc57c8ff860f8f42
Yubeen971 <92639469+Yubeen971@users.noreply.github.com>
2023-06-27 20:12:05 -0400

    pixelization (1/3)

--- Assets/Code/PixelizeFeature.cs
+++ Assets/Code/PixelizeFeature.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tommy : MonoBehaviour
+public class PixelizeFeature : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
